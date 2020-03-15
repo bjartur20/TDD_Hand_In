badb11584d458a293a85bd9a453c56999c4aa7f0
bjartur20 <bjartur20@gmail.com>
2020-03-15 18:56:13 +0000

    Added support for adding two numbers

--- kata.py
+++ kata.py
@@ -1,5 +1,12 @@
 def add(numbers):
-    if len(numbers) == 1:
-        return int(numbers)
     if not numbers:
         return 0
+
+    number_list = numbers.split(',')
+    sumation = sum(int(number) for number in number_list)
+
+    return sumation
+
+
+if __name__ == "__main__":
+    print(add('1'))
--- kata_test.py
+++ kata_test.py
@@ -1,8 +1,10 @@
 import kata
 
-
 def test_add_empty_string():
     assert kata.add('') == 0
 
 def test_single_number():
     assert kata.add('1') == 1
+
+def test_two_numbers():
+    assert kata.add("1,2") == 3
