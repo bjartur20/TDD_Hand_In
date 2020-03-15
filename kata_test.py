975986c1f4d718b5017961b0bf1e44fc8460226e
bjartur20 <bjartur20@gmail.com>
2020-03-15 18:58:24 +0000

    Added support for adding an unknown number of numbers (already worked before so no red tests)

--- kata_test.py
+++ kata_test.py
@@ -7,4 +7,8 @@ def test_single_number():
     assert kata.add('1') == 1
 
 def test_two_numbers():
-    assert kata.add("1,2") == 3
+    assert kata.add('1,2') == 3
+
+def test_unknown_number_of_numbers():
+    assert kata.add('1,2,3,4,5') == 15
+    assert kata.add('10,2,5,22,1,1') == 41
