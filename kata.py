790358356dce1780de5511f907ebd82df99afb51
bjartur20 <bjartur20@gmail.com>
2020-03-15 18:43:11 +0000

    Added support for single number

--- kata.py
+++ kata.py
@@ -1,3 +1,5 @@
 def add(numbers):
+    if len(numbers) == 1:
+        return int(numbers)
     if not numbers:
         return 0
--- kata_test.py
+++ kata_test.py
@@ -3,3 +3,6 @@ import kata
 
 def test_add_empty_string():
     assert kata.add('') == 0
+
+def test_single_number():
+    assert kata.add('1') == 1
